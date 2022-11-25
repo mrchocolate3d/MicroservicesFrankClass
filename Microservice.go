fec11cde63c7a499ec6f9b3446a9c6a67f9f08be
mrchocolate3d <43957746+mrchocolate3d@users.noreply.github.com>
2022-11-25 11:49:43 +0100

    Add files via upload

--- Microservice.go
+++ Microservice.go
@@ -25,7 +25,7 @@ func main() {
 	var router = mux.NewRouter()
 	router.HandleFunc("/customers", customers).Methods("GET").Name("customers")
 
-	err := http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":4000", router)
 	if err != nil {
 		log.Fatal(err)
 	}
